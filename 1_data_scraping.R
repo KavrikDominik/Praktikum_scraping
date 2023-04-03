a96d95e960fe4fdb5ef2476b1fe54fcdac227836
Dominik Kavrik <dominik@eduroam147-022.nbk.vse.cz>
2023-04-03 10:29:22 +0200

    LS2023 update

--- 1_data_scraping.R
+++ 1_data_scraping.R
@@ -47,13 +47,13 @@ for (model in model_vec){
       model = data$results$model_cb$seo_name,
       model_id = data$results$model_cb$value,
       name = data$results$category$seo_name,
+      additional_model_name = data$results$additional_model_name,
       price = data$results$price,
       miliage = data$results$tachomete,
       price_by_agreement = data$results$price_by_agreement,
       topped = data$results$topped,
       premise = data$results$premise$name,
       id = data$results$id,
-      additional = data$results$additional_model_name,
       fuel = data$results$fuel_cb$s,
       fuel_id = data$results$fuel_cb$value,
       gearbox = data$results$gearbox_cb$seo_name,
@@ -82,11 +82,6 @@ for (model in model_vec){
 }
 # data <- 
   output %>% 
-  select(name, model_cb, fuel_cb, gearbox_cb, additional_model_name,
-         tachometer, price, premise, locality, in_operation_date, images) %>% 
-  mutate(img_url=map(images, .f = ~lapply(.x, '[[', 1))) %>% 
-  mutate(img_url=map(img_url, .f = ~unlist(.x))) %>% unnest(img_url) %>%
-  mutate(img_url = paste0("http:",img_url, suffix)) %>% 
   mutate(additional_model_name = tolower(additional_model_name)) %>% 
   mutate(kombi = ifelse(str_detect(.$additional_model_name, "kombi|combi"),1,0)) %>%
   mutate(additional_model_name = str_replace(additional_model_name, "(?<=\\d)\\,+(?=\\d)", "\\.")) %>% 
@@ -95,39 +90,10 @@ for (model in model_vec){
   mutate(now = Sys.Date()) %>% 
   mutate(age = time_length(
     difftime(as.Date(now), in_operation_date),"years") ) %>% 
-  select(price,model_cb, objem, age, tachometer, fuel_cb, gearbox_cb, kombi, locality,img_url) %>% 
-  unnest() %>% 
   mutate(across(.cols = everything(), ~ifelse(.=="", NA, .))) %>%
-  select(-c(value, name1, seo_name1, value1, name, housenumber, municipality,  
-            municipality_id, entity_type,source, street, street_id, streetnumber,
-            quarter, quarter_id, municipality_seo_name, ward_id, ward, citypart,
-            country_id,address ,address_id, zip, region, region_id, region_seo_name,name2, seo_name2)) %>% 
   mutate(objem = as.numeric(objem)) %>% 
-  na.omit() %>% 
-  mutate(index = row_number())
-
-model_names <- output %>% 
-  select(name, model_cb, fuel_cb, gearbox_cb, additional_model_name,
-         tachometer, price, premise, locality, in_operation_date, images) %>% 
-  mutate(img_url=map(images, .f = ~lapply(.x, '[[', 1))) %>% 
-  mutate(img_url=map(img_url, .f = ~unlist(.x))) %>% unnest(img_url) %>%
-  mutate(img_url = paste0("http:",img_url, suffix)) %>% 
-  mutate(additional_model_name = tolower(additional_model_name)) %>% 
-  mutate(kombi = ifelse(str_detect(.$additional_model_name, "kombi|combi"),1,0)) %>%
-  mutate(additional_model_name = str_replace(additional_model_name, "(?<=\\d)\\,+(?=\\d)", "\\.")) %>% 
-  mutate(objem = str_extract(additional_model_name, "[1-3]\\.\\d{1,1}")) %>%
-  mutate(in_operation_date = as.Date(in_operation_date)) %>% 
-  mutate(now = Sys.Date()) %>% 
-  mutate(age = time_length(
-    difftime(as.Date(now), in_operation_date),"years") ) %>% 
-  select(model_cb, img_url) %>% unnest() %>% rename(model = seo_name) %>%  select(model, img_url) %>%  na.omit()
+  na.omit()
 
 
-data <- data %>% 
-  left_join(model_names, by = "img_url") %>% 
-  select(model, 1:12)
-
-
-
-write.csv(data, "data_skoda.csv", row.names = FALSE)
+# write.csv(data, "data_skoda.csv", row.names = FALSE)
  
