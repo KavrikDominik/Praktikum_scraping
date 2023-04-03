eff69030256d78d8e16e3bc585668408e94c0b8b
Dominik Kavrik <dominik@eduroam147-022.nbk.vse.cz>
2023-04-03 10:59:35 +0200

    ls2023 update

--- config/packages.R
+++ config/packages.R
@@ -2,7 +2,7 @@ if(!("yaml" %in% installed.packages())){
   install.packages("yaml")
 }
 
-package_list <- yaml::read_yaml("week_6/config/packages.yml")
+package_list <- yaml::read_yaml("config/packages.yml")
 
 new_packages <- package_list[!(package_list %in% installed.packages()[,"Package"])]
 if(length(new_packages)){
